e98f5b51b84e11ace678582eb3c58b9d6c938782
Josh Bryan <jbryan@ci.uchicago.edu>
2012-09-20 14:38:28 -0500

    Started splitting out authentication mechanisms for base client

---
+++ src/main/java/org/globusonline/transfer/X509Authenticator.java
@@ -0,0 +1,12 @@
+package org.globusonline.transfer;
+
+import javax.net.ssl.HttpsURLConnection;
+
+public class X509Authenticator implements Authenticator {
+
+	public void authenticateConnection(HttpsURLConnection c) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
